e51ad52ac393f20e24f5e950ea62aa457cb168f9
Arthur Escalera <arthurescalera@gmail.com>
2020-11-19 15:19:57 -0300

    correções da aula

--- login/index.php
+++ login/index.php
@@ -1,17 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-<div class="container col-6 bg-dark text-light">
-    <form action="" class="form-group"></form>
-</div>
+<?php
 
-<?php ?>
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-</body>
-</html>
+session_start();
+
+require_once 'db.php';
+
+//Exemplo de como fazer o salt na senha
+//$pass = password_hash('123456', PASSWORD_DEFAULT);
+
+if ( isset($_SESSION['login']) ) { //Caso o usuário já esteja logado no sistema
+
+		include 'header_tpl.php';
+		include 'index_menu_tpl.php';
+		include 'footer_tpl.php';
+
+} elseif ( isset( $_POST['entrar'] ) ) { //Caso o usuário tenha acabado de preencher o form de login
+
+	$login = filter_var( $_POST['login'], FILTER_SANITIZE_EMAIL);
+	$senha = $_POST['senha'];
+
+	//Verficar se existe o usuário e pegar o hash da senha
+	$r = $db->query("SELECT senha FROM usuario WHERE email = '$login'"); //senha é uma PK para pegar da tabela usuário o email que for igual ao colocado no form de login e que entrou aqui na variavel $login
+	$reg = $r->fetch(PDO::FETCH_ASSOC);
+	$hash = $reg['senha'];
+
+	//Comprara a senha passada pelo usuário com o hash armazenado no DB
+	if ( password_verify( $senha, $hash) ) {
+
+		$_SESSION['login'] = $login;
+
+		include 'header_tpl.php';
+		include 'index_menu_tpl.php';
+		include 'footer_tpl.php';
+
+	} else {
+
+		$msg = 'Credenciais inválidas, tente novamente';
+
+		include 'index_tpl.php';		
+	}
+
+} else { //Caso o usuário tenha acabado de chegar no sistema
+
+	include 'index_tpl.php';
+}
